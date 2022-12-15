a61861b662d542256e83bd823482502bf8c3f2a3
shruthib23 <116623593+shruthib23@users.noreply.github.com>
2022-12-15 21:38:49 +0530

    Add files via upload

---
+++ Problem2.java
@@ -0,0 +1,81 @@
+package javaPrograms;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+ class Country {
+ HashSet<String> H1 = new HashSet<>();
+ 
+ public HashSet<String> saveCountryNames(String CountryName) {
+  H1.add(CountryName);
+  return H1;
+ }
+ 
+ public String getCountry(String CountryName) {
+  Iterator<String> it = H1.iterator();
+  
+  while (it.hasNext()) {
+   if (it.next().equals(CountryName))
+    return CountryName;
+  }
+  
+  return null;
+ }
+}
+public class Problem2 {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  Country countries = new Country();
+  countries.saveCountryNames("India");
+  countries.saveCountryNames("USA");
+  countries.saveCountryNames("Pakistan");
+  countries.saveCountryNames("Bangladesh");
+  countries.saveCountryNames("China");
+
+  System.out.println("China: " + countries.getCountry("China"));
+  System.out.println("Japan: " + countries.getCountry("Japan"));  
+
+ }
+
+}package javaPrograms;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+ class Country {
+ HashSet<String> H1 = new HashSet<>();
+ 
+ public HashSet<String> saveCountryNames(String CountryName) {
+  H1.add(CountryName);
+  return H1;
+ }
+ 
+ public String getCountry(String CountryName) {
+  Iterator<String> it = H1.iterator();
+  
+  while (it.hasNext()) {
+   if (it.next().equals(CountryName))
+    return CountryName;
+  }
+  
+  return null;
+ }
+}
+public class Problem2 {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  Country countries = new Country();
+  countries.saveCountryNames("India");
+  countries.saveCountryNames("USA");
+  countries.saveCountryNames("Pakistan");
+  countries.saveCountryNames("Bangladesh");
+  countries.saveCountryNames("China");
+
+  System.out.println("China: " + countries.getCountry("China"));
+  System.out.println("Japan: " + countries.getCountry("Japan"));  
+
+ }
+
+}
